ec5b671c608d6ae25f415e2d4cba32b7b565c42a
v-lavs <lavskavanda@gmail.com>
2021-09-03 12:31:47 +0300

    add section banner

--- src/js/main.js
+++ src/js/main.js
@@ -50,7 +50,9 @@ $(document).ready(function () {
         // DESTROY SLIDER INSTANCES
         if ($(window).outerWidth() <= 991) {
             if (!reasonSlider && reasonSelector) {
-                reasonSlider = reasonSlider = new Swiper(".reason-slider", {
+                reasonSlider = new Swiper(".reason-slider", {
+                    spaceBetween: 60,
+                    slidesPerView: 1,
                     pagination: {
                         el: ".swiper-pagination",
                         clickable: true,
